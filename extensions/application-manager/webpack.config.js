e218eaadf7d6c399c0383cc671aeceb93ce44db7
wufan12 <wufan12@kuaishou.com>
2024-02-01 19:25:41 +0800

    feat: 提交

---
+++ extensions/application-manager/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+const tsConfigPath = path.join(__dirname, 'tsconfig.json');
+const config = {
+  target: 'node',
+  entry: './src/extension.ts',
+  node: {
+    __dirname: false,
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              configFile: tsConfigPath,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader',
+      },
+    ],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+
+  return config;
+};
---
+++ packages/common-service/CHANGELOG.md
@@ -0,0 +1,14 @@
+# CHANGELOG
+
+## 0.1.1
+
+- [chore]: update taobao npm registry from `https://registry.npm.taobao.org` to `https://registry.npmmirror.com`
+
+## 0.2.1
+
+- feat: add openFileInEditor
+
+## 0.2.0
+
+- refactor: clearer methods division
+- refactor: move methods which are about user to @appworks/user-service
