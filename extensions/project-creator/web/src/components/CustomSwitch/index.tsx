8582a4223c75381d11dde2effbb6375b542d79a6
wufan12 <wufan12@kuaishou.com>
2024-02-21 20:28:49 +0800

    feat: 删除create-project

--- extensions/project-creator/web/src/components/CustomSwitch/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Switch } from '@alifd/next';
-
-const SwitchComponent = ({ checked, onChange, size }) => (
-  <Switch checked={checked} onChange={onChange} size={size || 'medium'} />
-);
-
-export default SwitchComponent;
--- extensions/project-creator/web/src/pages/CreateProject/constants.ts
+++
@@ -1,5 +0,0 @@
-export const scaffoldsBlackList = [
-  '@icedesign/scaffold-midway-faas',
-  '@rax-materials/scaffolds-app-midway-faas',
-  '@rax-materials/scaffolds-app-midway-ssr',
-];
